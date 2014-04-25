158deaf696c365bac5ae6282a33c1920719dabc7
somtomking <liust@foxmail.com>
2014-04-25 16:28:46 +0800

    zxcvzxcv

---
+++ Source/Exp.Study/EF/Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Exp.Study.EF
+{
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            string connectionStr=string.Empty;
+            DbContext db = new DbContext(connectionStr);
+            
+            
+        }
+    }
+}
