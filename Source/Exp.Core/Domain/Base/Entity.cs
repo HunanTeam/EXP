0084a9ca4d33dd323a81ea707c88b9011c1a7899
somtomking <liust@foxmail.com>
2014-04-26 23:25:16 +0800

    科技

--- Source/Exp.Core/Domain/Base/Entity.cs
+++ Source/Exp.Core/Domain/Base/Entity.cs
@@ -5,12 +5,11 @@ using System.Text;
 
 namespace Exp.Core.Domain.Base
 {
+    /// <summary>
+    /// 不能在构造函数里赋值
+    /// </summary>
     public class Entity : EntityBase<int>
     {
-        public Entity()
-        {
-            this.CreateOn = DateTime.Now;
-        }
         public DateTime CreateOn { get; set; }
     }
 }
