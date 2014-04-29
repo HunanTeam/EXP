2f9f85a6797cfd85b834fc99a9cc098664b2e9a4
somtomking <liust@foxmail.com>
2014-04-29 23:04:50 +0800

    添加权限接口

---
+++ Source/Exp.Services/System/IUserService.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Exp.Core;
+using Exp.Core.Domain.System;
+using System;
+using System.Linq;
+ 
+
+
+namespace Exp.Services.System
+{
+	/// <summary>
+    /// 服务层接口 —— IUserService
+    /// </summary>
+    public interface IUserService
+    {
+		#region 属性
+
+        IQueryable<User> Users { get; }
+
+        #endregion
+
+        #region 公共方法
+
+        OperationResult Insert(UserModel model);
+
+        OperationResult Update(User model);
+
+        OperationResult Update(UpdateUserModel model);
+
+		OperationResult Update(ChangePwdModel model);
+
+        /// <summary>
+        /// 逻辑删除
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        OperationResult Delete(UserModel model);
+
+        #endregion
+	}
+}
