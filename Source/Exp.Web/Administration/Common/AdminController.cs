15c306b82fe18c833ba866acf33644d2fb4bf58e
somtomking <liust@foxmail.com>
2014-04-30 14:33:17 +0800

    添加东西

---
+++ Source/Exp.Web/Administration/Common/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using Exp.Core.Domain.System;
+using Exp.Side.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exp.Admin.Common
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        protected User GetCurrentUser()
+        {
+            var user = SessionHelper.GetSession("CurrentUser") as User;
+            return user;
+        }
+
+        protected void CreateBaseData<T>(T model) where T : EntityCommon
+        {
+            var user = SessionHelper.GetSession("CurrentUser") as User;
+            if (user != null)
+            {
+                model.CreateId = user.Id;
+                model.CreateBy = user.LoginName;
+                model.CreateTime = DateTime.Now;
+                model.ModifyId = user.Id;
+                model.ModifyBy = user.LoginName;
+                model.ModifyTime = DateTime.Now;
+            }
+        }
+
+        protected void UpdateBaseData<T>(T model) where T : EntityCommon
+        {
+            var user = SessionHelper.GetSession("CurrentUser") as User;
+            if (user != null)
+            {
+                model.ModifyId = user.Id;
+                model.ModifyBy = user.LoginName;
+                model.ModifyTime = DateTime.Now;
+            }
+        }
+    }
+}
