fc54fc3a2549996b64885971b1f13d1a66c3ef39
somtomking <liust@foxmail.com>
2014-05-08 15:58:16 +0800

    test

--- Source/Study/Exp.Study/Program.cs
+++ Source/Study/Exp.Study/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -10,26 +11,34 @@ namespace Exp.Study
         static void Main(string[] args)
         {
 
-            string[] formats = new string[] { "N", "C", "E", "F", };
+            string[] formats = new string[] { "N", "C", "E", "F", "G", "G28" };
             var number = 123.44m;
+            var number2 = 123m;
+            System.Threading.Thread.CurrentThread.CurrentCulture= new CultureInfo("Zh-cn");
+            
             foreach (var format in formats)
             {
 
-               
-               Console.WriteLine(string.Format(" {0} is {1}", format, number.ToString(format)));
+
+                Console.WriteLine(string.Format(" {0} is {1}    {2}", format, number.ToString(format), number2.ToString(format)));
 
             }
+            int[] data = new int[] { 1,2,3,4,5,6,7,89};
+            var result = string.Join(",", data);
+            
+            Console.WriteLine(result);
+
             Console.Read();
         }
 
-       
+
     }
 
     public class NumberFormatHelper
     {
-       
-        
+
+
     }
 
-    
+
 }
