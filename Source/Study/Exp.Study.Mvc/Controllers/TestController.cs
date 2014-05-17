0da68b74cb9cebc50cfb885398e9e9b93a9fd030
somtomking <liust@foxmail.com>
2014-05-17 15:19:28 +0800

    ddd

---
+++ Source/Study/Exp.Study.Mvc/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exp.Study.Mvc.Views
+{
+    public class TestController : Controller
+    {
+        #region Nav
+        public ActionResult Index()
+        {
+
+            return View();
+
+        }
+        #endregion
+        /// <summary>
+        /// JS替换占位符（同时加载JS）
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ReplaceHolder()
+        {
+            return View();
+        }
+        
+        public ActionResult PlaceHolderContent()
+        {
+            return PartialView();
+        }
+    }
+}
