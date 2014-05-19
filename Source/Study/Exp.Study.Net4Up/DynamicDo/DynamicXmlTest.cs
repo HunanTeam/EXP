05a514581902738f94fd8d6da919640031cd62df
somtomking <liust@foxmail.com>
2014-05-19 16:01:22 +0800

    dddd

---
+++ Source/Study/Exp.Study.Net4Up/DynamicDo/DynamicXmlTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp.Study.Net4Up.DynamicDo
+{
+    class DynamicXmlTest
+    {
+        static void Main(string[] args)
+        {
+            var xmlStr = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DynamicDo", "DynamicXm.xml"));
+            dynamic xml=new DynamicXml (xmlStr);
+            
+
+        }
+    }
+}
---
+++ Source/Study/Exp.Study.Net4Up/IQueryableDo/IQueryableTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+namespace Exp.Study.Net4Up.IQueryableDo
+{
+    class IQueryableTest
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
