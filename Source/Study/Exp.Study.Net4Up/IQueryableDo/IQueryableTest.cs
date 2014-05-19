9b8aa75b84ddebcb03fe1e142e60f29ee1382c4b
somtomking <liust@foxmail.com>
2014-05-19 16:16:51 +0800

    fasdfasdfasdf

--- Source/Study/Exp.Study.Net4Up/IQueryableDo/IQueryableTest.cs
+++ Source/Study/Exp.Study.Net4Up/IQueryableDo/IQueryableTest.cs
@@ -13,4 +13,34 @@ namespace Exp.Study.Net4Up.IQueryableDo
             
         }
     }
+
+    public class QueryableImpl : IQueryable<T>
+    {
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type ElementType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public System.Linq.Expressions.Expression Expression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public System.Linq.IQueryProvider Provider
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
 }
